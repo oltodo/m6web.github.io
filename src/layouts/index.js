badbcf1c2910db783b93b0f03a666690cc31b6db
Nicolas Bazille <nicolas.bazille@m6.fr>
2017-07-19 13:15:38 +0200

    Prettierize

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,14 +1,14 @@
-import React from "react"
-import PropTypes from "prop-types"
-import Link from "gatsby-link"
-import Helmet from "react-helmet"
+import React from "react";
+import PropTypes from "prop-types";
+import Link from "gatsby-link";
+import Helmet from "react-helmet";
 
-import "../css/typography.css"
+import "../css/typography.css";
 
 export default class Template extends React.Component {
   static propTypes = {
-    children: PropTypes.func,
-  }
+    children: PropTypes.func
+  };
 
   render() {
     return (
@@ -17,20 +17,20 @@ export default class Template extends React.Component {
           title="Gatsby Default Starter"
           meta={[
             { name: "description", content: "Sample" },
-            { name: "keywords", content: "sample, something" },
+            { name: "keywords", content: "sample, something" }
           ]}
         />
         <div
           style={{
             background: `rebeccapurple`,
-            marginBottom: `1.45rem`,
+            marginBottom: `1.45rem`
           }}
         >
           <div
             style={{
               margin: `0 auto`,
               maxWidth: 960,
-              padding: `1.45rem 1.0875rem`,
+              padding: `1.45rem 1.0875rem`
             }}
           >
             <h1 style={{ margin: 0 }}>
@@ -38,7 +38,7 @@ export default class Template extends React.Component {
                 to="/"
                 style={{
                   color: "white",
-                  textDecoration: "none",
+                  textDecoration: "none"
                 }}
               >
                 Gatsby
@@ -51,12 +51,12 @@ export default class Template extends React.Component {
             margin: `0 auto`,
             maxWidth: 960,
             padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
+            paddingTop: 0
           }}
         >
           {this.props.children()}
         </div>
       </div>
-    )
+    );
   }
 }
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,6 +1,6 @@
-import React from "react"
-import Link from "gatsby-link"
-import Helmet from "react-helmet"
+import React from "react";
+import Link from "gatsby-link";
+import Helmet from "react-helmet";
 
 export default class Page2 extends React.Component {
   render() {
@@ -10,6 +10,6 @@ export default class Page2 extends React.Component {
         <p>Welcome to page 2</p>
         <Link to="/">Go back to the homepage</Link>
       </div>
-    )
+    );
   }
 }
